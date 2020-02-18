ca6b9e5ce0682106fce5ed57b95348cbe0cf7ec2
Eugen Morozov <meugenom@gmail.com>
2020-02-18 18:52:56 +0100

    added simple node server for testing
    added simple node server for testing

---
+++ simple-server.js
@@ -0,0 +1,16 @@
+// import http module
+const http = require('http');
+
+// server configuration
+const HOST = '127.0.0.1';
+const PORT = 8080;
+
+// create the server
+const server = http.createServer((req, res) => {
+  res.end('Hello!');
+});
+
+// make the server listen to requests
+server.listen(PORT, HOST, () => {
+  console.log(`Server running at: http://${HOST}:${PORT}/`);
+});
